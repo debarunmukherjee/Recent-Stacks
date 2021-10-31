6e097fd36e290a6b2ffcca7db01e103e1423b6e0
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-10-31 12:16:58 +0530

    Add retrofit helper class

---
+++ app/src/main/java/com/example/recentstacks/api/RetrofitHelper.kt
@@ -0,0 +1,15 @@
+package com.example.recentstacks.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+    private const val BASE_URL = "https://api.stackexchange.com"
+
+    fun getInstance() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
